82e1ff0fcaa72dee7537b6dc8cc8c60e7f1ea245
rohit-kumar-72 <rohitraj72342@gmail.com>
2024-10-07 00:16:14 +0530

    update cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || '*',
 }));
 
 app.use(express.json({ limit: "20kb" }));
